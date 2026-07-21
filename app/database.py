ed547dcd9d747757925b545fe4ac15f3ac813680
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-21 22:56:18 +0200

    feat: configure async engine and sessionmaker in database module

--- app/database.py
+++ app/database.py
@@ -1,4 +1,13 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+
 
 class Base(DeclarativeBase):
     pass
+
+
+DATABASE_URL = 'postgresql+asyncpg://postgres:postgres@localhost:5432/smartflow_db'
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
