b7f4eb01359842b16665d17b5927439b3114c134
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-24 23:24:54 +0200

    feat: add JWT access token creation in security module

--- app/security.py
+++ app/security.py
@@ -1,4 +1,12 @@
 from passlib.context import CryptContext
+import jwt
+from datetime import datetime, timezone, timedelta
+
+SECRET_KEY = 'my_secret_key'
+
+ALGORITHM = 'HS256'
+
+ACCESS_TOKEN_EXPIRES_MINUTES = 30
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -10,3 +18,11 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    exp = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
+    to_encode.update({'exp': exp})
+    jwt_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt_token
