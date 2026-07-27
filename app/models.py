9c7e30ed277f9ba1d6219fd24416a456f3168dfa
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-27 20:35:23 +0200

    feat: update User model with telegram_id and optional email

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
-from sqlalchemy import String, Boolean
+from sqlalchemy import Boolean, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
+from typing import Optional
 
 class User(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(unique=True, index=True)
-    hashed_password: Mapped[str]
+    email: Mapped[Optional[str]] = mapped_column(unique=True, index=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, index=True)
+    hashed_password: Mapped[Optional[str]]
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
