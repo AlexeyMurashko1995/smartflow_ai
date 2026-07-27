4d8147b704ace3eae76134042fbb0873c82a7d59
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-27 21:05:48 +0200

    feat: add /telegram-login endpoint

--- app/users.py
+++ app/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
-from app.schemas import UserCreate, UserResponse, Token
+from app.schemas import UserCreate, UserResponse, Token, UserTelegramAuth
 from app.database import get_async_session, AsyncSession
 from app.models import User
 from app.security import get_password_hash, verify_password, create_access_token, get_current_user
@@ -42,3 +42,17 @@ async def get_login(
 @router.get('/me', response_model=UserResponse)
 async def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
+
+
+@router.post('/telegram-login', response_model=Token)
+async def telegram_login(user_data: UserTelegramAuth, session: AsyncSession = Depends(get_async_session)):
+    query = select(User).where(User.telegram_id==user_data.telegram_id)
+    result = await session.execute(query)
+    target_user = result.scalar_one_or_none()
+    if target_user is None:
+        target_user = User(telegram_id=user_data.telegram_id)
+        session.add(target_user)
+        await session.commit()
+        await session.refresh(target_user)
+    jwt_token = create_access_token(data={'sub': str(target_user.telegram_id)})
+    return {'access_token': jwt_token, 'token_type': 'bearer'}
