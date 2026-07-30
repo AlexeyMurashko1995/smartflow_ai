ee8829f21f3193631842d51df0adcadd3588f85d
AlexMurashko <alexeymurashko1995@gmail.com>
2026-07-30 22:15:41 +0200

    feat: add login_telegram method signature to APIClient

--- bot/client.py
+++ bot/client.py
@@ -4,3 +4,5 @@ from bot.config import bot_settings
 class APIClient():
     def __init__(self):
         self.base_url = bot_settings.API_BASE_URL
+    async def login_telegram(self, telegram_id: int):
+        pass
