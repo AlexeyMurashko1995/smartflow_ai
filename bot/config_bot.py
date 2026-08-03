d668d9f9aefcfbda84cbd0ec29b3da5506ab9026
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-03 21:55:36 +0200

    fix: add extra='ignore' to pydantic settings configs

---
+++ bot/config_bot.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    API_BASE_URL: str = 'http://127.0.0.1:8000'
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+
+bot_settings = Settings()
