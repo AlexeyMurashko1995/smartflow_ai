3923a5c1f83304c54fc6d6662e8de93ee133b831
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-04 21:25:42 +0200

    feat: add APIClient with async telegram login method

--- bot/client.py
+++ bot/client.py
@@ -5,4 +5,6 @@ class APIClient():
     def __init__(self):
         self.base_url = bot_settings.API_BASE_URL
     async def login_telegram(self, telegram_id: int):
-        pass
+        async with httpx.AsyncClient() as client:
+            response = await client.post( url=f'{self.base_url}/users/telegram-login', json={'telegram_id': telegram_id})
+        return response.json()
