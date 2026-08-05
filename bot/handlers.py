346ee24923e3506c54b098b40277a402df860843
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-05 21:32:48 +0200

    feat: add start command handler

---
+++ bot/handlers.py
@@ -0,0 +1,15 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from bot.client import APIClient
+
+router = Router()
+
+api_client = APIClient()
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+  telegram_id = message.from_user.id
+  data = await api_client.login_telegram(telegram_id)
+  await message.answer(f'Hi! Token received')
