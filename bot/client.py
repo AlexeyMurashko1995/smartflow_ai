b08f4fc7be77ca53abf55c7d90d58051574cd383
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-05 22:32:39 +0200

    refactor: format client.py according to pep-8

--- bot/client.py
+++ bot/client.py
@@ -1,23 +1,43 @@
 import httpx
+
 from bot.config_bot import bot_settings
 
-class APIClient():
+
+class APIClient:
+
     def __init__(self):
         self.base_url = bot_settings.API_BASE_URL
+
     async def login_telegram(self, telegram_id: int):
         async with httpx.AsyncClient() as client:
-            response = await client.post(url=f'{self.base_url}/users/telegram-login', json={'telegram_id': telegram_id})
-        return response.json()
+            response = await client.post(
+                url=f'{self.base_url}/users/telegram-login',
+                json={'telegram_id': telegram_id},
+            )
+            return response.json()
 
     async def get_categories(self, access_token: str):
         async with httpx.AsyncClient() as client:
             headers = {'Authorization': f'Bearer {access_token}'}
-            response = await client.get(url=f'{self.base_url}/categories/', headers=headers)
+            response = await client.get(
+                url=f'{self.base_url}/categories/',
+                headers=headers,
+            )
             return response.json()
 
-    async def add_expense(self, access_token: str, amount: float, category_id: int, description: str = None):
+    async def add_expense(
+        self,
+        access_token: str,
+        amount: float,
+        category_id: int,
+        description: str | None = None,
+    ):
         async with httpx.AsyncClient() as client:
             headers = {'Authorization': f'Bearer {access_token}'}
             payload = {'amount': amount, 'category_id': category_id, 'description': description}
-            response = await client.post(url=f'{self.base_url}/expenses/', headers=headers, json=payload)
+            response = await client.post(
+                url=f'{self.base_url}/expenses/',
+                headers=headers,
+                json=payload,
+            )
             return response.json()
