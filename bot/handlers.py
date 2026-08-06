700cc57367813c483dda058f64cfea70a647d3a4
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-06 21:54:28 +0200

    feat: save JWT token to FSMContext in start handler

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,5 +1,6 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from bot.client import APIClient
@@ -9,8 +10,9 @@ api_client = APIClient()
 
 
 @router.message(CommandStart())
-async def cmd_start(message: Message):
+async def cmd_start(message: Message, state: FSMContext):
     telegram_id = message.from_user.id
     data = await api_client.login_telegram(telegram_id)
     access_token = data['access_token']
-    await message.answer(f'Hi! Token received. Your token: {access_token[:10]}')
+    await state.update_data(jwt_token=access_token)
+    await message.answer(f'Hi! Token received. Your token: {access_token[:10]}. The token was saved.')
