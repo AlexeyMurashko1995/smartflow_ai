42dfaaf69435d20686076a8bf7aef122662d7635
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-10 21:50:26 +0200

    feat: add process_amount handler with category keyboard

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,8 +1,9 @@
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from bot.client import APIClient
 
@@ -40,3 +41,27 @@ async def get_categories(message: Message, state: FSMContext) -> None:
 async def add_expense(message: Message, state: FSMContext) -> None:
     await state.set_state(AddExpenseStates.waiting_for_amount)
     await message.answer('Enter the amount: ')
+
+
+@router.message(AddExpenseStates.waiting_for_amount)
+async def process_amount(message: Message, state: FSMContext) -> None:
+    try:
+        amount = float(message.text)
+        if amount <= 0:
+            raise ValueError
+        await state.update_data(amount=amount)
+        data_user = await state.get_data()
+        jwt_token = data_user.get('jwt_token')
+        builder = InlineKeyboardBuilder()
+        categories = await api_client.get_categories(jwt_token)
+        for cat in categories:
+            builder.button(
+                text=cat['name'],
+                callback_data=f'category_{cat['id']}'
+            )
+        builder.adjust(2)
+        await message.answer('Choose a category: ', reply_markup=builder.as_markup())
+        await state.set_state(AddExpenseStates.waiting_for_category_id)
+    except ValueError:
+        await message.answer('Please enter a valid amount: ')
+        return
