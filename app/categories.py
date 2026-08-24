10539c5f52c30d74b4f4886d21375954f9ca364d
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-24 22:32:04 +0200

    feat: add redis cache to GET categories

--- app/categories.py
+++ app/categories.py
@@ -1,8 +1,10 @@
+import json
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from app.schemas import CategoryCreate, CategoryResponse
 from app.database import get_async_session, AsyncSession
 from app.models import Category, User
+from app.redis_client import redis_client
 from app.security import get_current_user
 
 router = APIRouter(prefix='/categories', tags=['Categories'])
@@ -19,7 +21,13 @@ async def create_category(category_data: CategoryCreate, session: AsyncSession =
 
 @router.get('/', response_model=list[CategoryResponse])
 async def get_all_categories(session: AsyncSession = Depends(get_async_session), current_user: User = Depends(get_current_user)):
+    cache_key = f"categories:{current_user.id}"
+    cached = await redis_client.get(cache_key)
+    if cached is not None:
+        return json.loads(cached)
     query = select(Category).where(current_user.id == Category.user_id)
     result = await session.execute(query)
     categories_list = result.scalars().all()
+    data_to_cache = [{"id": c.id, "name": c.name} for c in categories_list]
+    await redis_client.set(cache_key, json.dumps(data_to_cache), ex=300)
     return categories_list
