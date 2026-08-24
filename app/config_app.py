c52c6faddee4104af33d4d265f7324d096989ad9
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-24 21:16:32 +0200

    feat: add REDIS_URL to app settings

--- app/config_app.py
+++ app/config_app.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRES_MINUTES: int = 30
+    REDIS_URL: str = 'redis://localhost:6379/0'
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
 settings = Settings()
