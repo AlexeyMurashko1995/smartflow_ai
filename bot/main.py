24ab3166234250a6f593097eb1408bbed0f5fb0a
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-28 22:16:49 +0200

    feat: add bot entrypoint and update docker-compose command

---
+++ bot/main.py
@@ -0,0 +1,14 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from bot.config_bot import bot_settings
+from bot.handlers import router
+
+async def main():
+    bot = Bot(token=bot_settings.BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
